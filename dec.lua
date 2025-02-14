688850edaee843c5bc3fd00e753a0ebf76b178f0
Krixx <161109688+Krixx-XD@users.noreply.github.com>
2025-02-14 10:40:32 +0500

    Create dec.lua

---
+++ dec.lua
@@ -0,0 +1,15 @@
+(function(f)
+    local P = f:find("")
+    local y = 100 * 10 + 1
+    local g = y - 2
+
+    if P then
+        if (900 + #f > y + y + y + y + y - 1) then
+            local t = f
+            f = _G[string.gsub("::st:ri::ng:", ":", "")]
+            if f ~= "string" then
+                return
+            end
+        end
+    end
+end)()
