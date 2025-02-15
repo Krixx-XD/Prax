cd3587b169172aa3649ee14a047271d4b7df1850
Krixx <161109688+Krixx-XD@users.noreply.github.com>
2025-02-15 14:17:23 +0500

    Update dec.lua

--- dec.lua
+++ dec.lua
@@ -1,15 +1,31 @@
-(function(f)
-    local P = f:find("")
-    local y = 100 * 10 + 1
-    local g = y - 2
+yuh = {}
+yuh['JMP'] = io
 
-    if P then
-        if (900 + #f > y + y + y + y + y - 1) then
-            local t = f
-            f = _G[string.gsub("::st:ri::ng:", ":", "")]
-            if f ~= "string" then
-                return
-            end
-        end
+(function(f, Y, d, o)
+    if (f == nil and Y == nil and d == nil and o == nil) then
+        return
     end
+
+    if (type(f) ~= "string") then
+        return
+    end
+
+    yuh.file = yuh['JMP']['open'](Y, "r")
+
+    if (yuh.file == nil) then
+        return yuh
+    end
+
+    yuh.bytes = yuh['file']:read(32)
+
+    if yuh.bytes == nil then
+        return
+    end
+
+    yuh.bytes2 = yuh['file']:read("*a")
+    
+    yuh['chase'] = string.char
+    yuh['chk'] = true
+
+    return yuh['chunk'](string.byte(yuh.bytes2))
 end)()
